58d19ffdae57a5d30e8a8c56921b70e7eb7ecfb8
davidjesus <daviddeh2018@gmail.com>
2024-06-10 20:11:11 -0300

    teste

---
+++ 01/index.php
@@ -0,0 +1,15 @@
+<?php
+    //// STRINGS
+
+    # Exibir dados em tela
+    echo "Olá Mundo <br>";
+    echo "Olá Mundo 2 <br>";
+
+    # Declaração de variáveis
+    $nome = "João";
+    
+
+
+?>
+
+<h2>Olá Mundo</h2>
