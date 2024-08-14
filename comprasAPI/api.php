6c851e66501527c94ca99cb0d2440f53d2860f08
deeh275 <daviddeh2018@gmail.com>
2024-08-13 21:53:30 -0300

    aula nova

--- comprasAPI/api.php
+++ comprasAPI/api.php
@@ -1,14 +1,22 @@
 <?php
 
 require_once 'DatabaseRepository.php';
-$database = new DatabaseRepository();
 
 $acao = $_GET['acao'];
 
 if($acao == 'listar') {
-    echo $database->getAllItems();
+    echo json_encode(DatabaseRepository::getAllItems());
 } else if($acao == 'adicionar') {
-    echo $database->addItem();
+    $data = json_decode(file_get_contents('php://input'), true);
+    echo DatabaseRepository::addItem($data['nome_produto'], $data['quantidade']);
+} else if($acao == 'atualizar') {
+    $id = $_GET['id'];
+    $data = json_decode(file_get_contents('php://input'), true);
+    //echo json_encode($data);
+    echo DatabaseRepository::updateItem($id, $data['nome_produto'], $data['quantidade'], $data['comprado']);
+}  else if($acao == 'deletar') {
+    $id = $_GET['id'];
+    echo DatabaseRepository::deleteItem($id);    
 } else {
     echo "Ação não implementada";
 }
